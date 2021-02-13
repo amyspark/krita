754a8d730b036d4fa1729742261df9873aa1e30d
L. E. Segovia <amy@amyspark.me>
2021-02-13 01:22:31 +0000

    Upgrade LcmsColorSpace to C++14

--- plugins/color/lcms2engine/LcmsColorSpace.h
+++ plugins/color/lcms2engine/LcmsColorSpace.h
@@ -11,12 +11,12 @@
 #define KOLCMSCOLORSPACE_H_
 
 #include <array>
+#include <vector>
+#include <kis_assert.h>
 #include <kis_lockless_stack.h>
 #include <KoColorSpaceAbstract.h>
 
 #include "colorprofiles/LcmsColorProfileContainer.h"
-#include "kis_assert.h"
-
 
 class LcmsColorProfileContainer;
 
@@ -66,21 +66,19 @@ struct KoLcmsDefaultTransformations {
  * This is the base class for all colorspaces that are based on the lcms library, for instance
  * RGB 8bits and 16bits, CMYK 8bits and 16bits, LAB...
  */
-template<class _CSTraits>
-class LcmsColorSpace : public KoColorSpaceAbstract<_CSTraits>, public KoLcmsInfo
+template<class CSTraits>
+class LcmsColorSpace : public KoColorSpaceAbstract<CSTraits>, public KoLcmsInfo
 {
     struct KoLcmsColorTransformation : public KoColorTransformation {
 
         KoLcmsColorTransformation(const KoColorSpace *colorSpace)
             : KoColorTransformation()
             , m_colorSpace(colorSpace)
+            , csProfile(nullptr)
+            , profiles{nullptr, nullptr, nullptr}
+            , cmstransform(nullptr)
+            , cmsAlphaTransform(nullptr)
         {
-            csProfile = 0;
-            cmstransform = 0;
-            cmsAlphaTransform = 0;
-            profiles[0] = 0;
-            profiles[1] = 0;
-            profiles[2] = 0;
         }
 
         ~KoLcmsColorTransformation() override
@@ -102,15 +100,15 @@ class LcmsColorSpace : public KoColorSpaceAbstract<_CSTraits>, public KoLcmsInfo
 
         void transform(const quint8 *src, quint8 *dst, qint32 nPixels) const override
         {
-            cmsDoTransform(cmstransform, const_cast<quint8 *>(src), dst, nPixels);
+            cmsDoTransform(cmstransform, src, dst, nPixels);
 
             qint32 numPixels = nPixels;
             qint32 pixelSize = m_colorSpace->pixelSize();
             int index = 0;
 
             if (cmsAlphaTransform) {
-                qreal *alpha = new qreal[nPixels];
-                qreal *dstalpha = new qreal[nPixels];
+                std::vector<qreal> alpha(nPixels);
+                std::vector<qreal> dstalpha(nPixels);
 
                 while (index < nPixels) {
                     alpha[index] = m_colorSpace->opacityF(src);
@@ -118,14 +116,11 @@ class LcmsColorSpace : public KoColorSpaceAbstract<_CSTraits>, public KoLcmsInfo
                     index++;
                 }
 
-                cmsDoTransform(cmsAlphaTransform, const_cast<qreal *>(alpha), static_cast<qreal *>(dstalpha), nPixels);
+                cmsDoTransform(cmsAlphaTransform, alpha.data(), dstalpha.data(), nPixels);
                 for (int i = 0; i < numPixels; i++) {
                     m_colorSpace->setOpacity(dst, dstalpha[i], 1);
                     dst += pixelSize;
                 }
-
-                delete [] alpha;
-                delete [] dstalpha;
             } else {
                 while (numPixels > 0) {
                     qreal alpha = m_colorSpace->opacityF(src);
@@ -139,7 +134,7 @@ class LcmsColorSpace : public KoColorSpaceAbstract<_CSTraits>, public KoLcmsInfo
 
         const KoColorSpace *m_colorSpace;
         cmsHPROFILE csProfile;
-        cmsHPROFILE profiles[3];
+        std::array<cmsHPROFILE, 3> profiles;
         cmsHTRANSFORM cmstransform;
         cmsHTRANSFORM cmsAlphaTransform;
     };
@@ -155,9 +150,9 @@ class LcmsColorSpace : public KoColorSpaceAbstract<_CSTraits>, public KoLcmsInfo
         }
     };
 
-    typedef QSharedPointer<KisLcmsLastTransformation> KisLcmsLastTransformationSP;
+    using KisLcmsLastTransformationSP = QSharedPointer<KisLcmsLastTransformation>;
 
-    typedef KisLocklessStack<KisLcmsLastTransformationSP> KisLcmsTransformationStack;
+    using KisLcmsTransformationStack = KisLocklessStack<KisLcmsLastTransformationSP>;
 
     struct Private {
         KoLcmsDefaultTransformations *defaultTransformations;
@@ -176,7 +171,7 @@ protected:
                    cmsUInt32Number cmType,
                    cmsColorSpaceSignature colorSpaceSignature,
                    KoColorProfile *p)
-        : KoColorSpaceAbstract<_CSTraits>(id, name)
+        : KoColorSpaceAbstract<CSTraits>(id, name)
         , KoLcmsInfo(cmType, colorSpaceSignature)
         , d(new Private())
     {
@@ -185,7 +180,7 @@ protected:
         d->profile = asLcmsProfile(p);
         Q_ASSERT(d->profile);
         d->colorProfile = p;
-        d->defaultTransformations = 0;
+        d->defaultTransformations = nullptr;
     }
 
     ~LcmsColorSpace() override
@@ -238,17 +233,13 @@ public:
 
     bool profileIsCompatible(const KoColorProfile *profile) const override
     {
-        const IccColorProfile *p = dynamic_cast<const IccColorProfile *>(profile);
+        const auto *p = dynamic_cast<const IccColorProfile *>(profile);
         return (p && p->asLcms()->colorSpaceSignature() == colorSpaceSignature());
     }
 
     void fromQColor(const QColor &color, quint8 *dst, const KoColorProfile *koprofile = nullptr) const override
     {
-        std::array<float, 3> qcolordata;
-
-        qcolordata[2] = color.redF();
-        qcolordata[1] = color.greenF();
-        qcolordata[0] = color.blueF();
+        std::array<float, 3> qcolordata {static_cast<float>(color.blueF()), static_cast<float>(color.greenF()), static_cast<float>(color.redF()) };
 
         LcmsColorProfileContainer *profile = asLcmsProfile(koprofile);
         if (profile == nullptr) {
@@ -279,7 +270,7 @@ public:
 
     void toQColor(const quint8 *src, QColor *c, const KoColorProfile *koprofile = nullptr) const override
     {
-        std::array<float, 3> qcolordata;
+        std::array<float, 3> qcolordata{};
 
         LcmsColorProfileContainer *profile = asLcmsProfile(koprofile);
         if (profile == nullptr) {
@@ -310,21 +301,18 @@ public:
     KoColorTransformation *createBrightnessContrastAdjustment(const quint16 *transferValues) const override
     {
         if (!d->profile) {
-            return 0;
+            return nullptr;
         }
 
-        cmsToneCurve *transferFunctions[3];
-        transferFunctions[0] = cmsBuildTabulatedToneCurve16(0, 256, transferValues);
-        transferFunctions[1] = cmsBuildGamma(0, 1.0);
-        transferFunctions[2] = cmsBuildGamma(0, 1.0);
+        std::array<cmsToneCurve *, 3> transferFunctions {cmsBuildTabulatedToneCurve16(nullptr, 256, transferValues), cmsBuildGamma(nullptr, 1.0), cmsBuildGamma(nullptr, 1.0)};
 
-        KoLcmsColorTransformation *adj = new KoLcmsColorTransformation(this);
-        adj->profiles[1] = cmsCreateLinearizationDeviceLink(cmsSigLabData, transferFunctions);
+        auto *adj = new KoLcmsColorTransformation(this);
+        adj->profiles[1] = cmsCreateLinearizationDeviceLink(cmsSigLabData, transferFunctions.data());
         cmsSetDeviceClass(adj->profiles[1], cmsSigAbstractClass);
 
         adj->profiles[0] = d->profile->lcmsProfile();
         adj->profiles[2] = d->profile->lcmsProfile();
-        adj->cmstransform  = cmsCreateMultiprofileTransform(adj->profiles, 3, this->colorSpaceType(), this->colorSpaceType(),
+        adj->cmstransform  = cmsCreateMultiprofileTransform(adj->profiles.data(), 3, this->colorSpaceType(), this->colorSpaceType(),
                              KoColorConversionTransformation::adjustmentRenderingIntent(),
                              KoColorConversionTransformation::adjustmentConversionFlags());
         adj->csProfile = d->profile->lcmsProfile();
@@ -334,25 +322,20 @@ public:
     KoColorTransformation *createPerChannelAdjustment(const quint16 *const *transferValues) const override
     {
         if (!d->profile) {
-            return 0;
+            return nullptr;
         }
 
-        cmsToneCurve **transferFunctions = new cmsToneCurve*[ this->colorChannelCount()];
+        std::vector<cmsToneCurve *> transferFunctions(this->colorChannelCount());
 
         for (uint ch = 0; ch < this->colorChannelCount(); ch++) {
-            transferFunctions[ch] = transferValues[ch] ?
-                                    cmsBuildTabulatedToneCurve16(0, 256, transferValues[ch]) :
-                                    cmsBuildGamma(0, 1.0);
+            transferFunctions[ch] = transferValues[ch] ? cmsBuildTabulatedToneCurve16(nullptr, 256, transferValues[ch]) : cmsBuildGamma(nullptr, 1.0);
         }
 
-        cmsToneCurve **alphaTransferFunctions = new cmsToneCurve*[1];
-        alphaTransferFunctions[0] = transferValues[this->colorChannelCount()] ?
-                                    cmsBuildTabulatedToneCurve16(0, 256, transferValues[this->colorChannelCount()]) :
-                                    cmsBuildGamma(0, 1.0);
+        std::array<cmsToneCurve *, 1> alphaTransferFunctions {transferValues[this->colorChannelCount()] ? cmsBuildTabulatedToneCurve16(0, 256, transferValues[this->colorChannelCount()]) : cmsBuildGamma(nullptr, 1.0)};
 
-        KoLcmsColorTransformation *adj = new KoLcmsColorTransformation(this);
-        adj->profiles[0] = cmsCreateLinearizationDeviceLink(this->colorSpaceSignature(), transferFunctions);
-        adj->profiles[1] = cmsCreateLinearizationDeviceLink(cmsSigGrayData, alphaTransferFunctions);
+        auto *adj = new KoLcmsColorTransformation(this);
+        adj->profiles[0] = cmsCreateLinearizationDeviceLink(this->colorSpaceSignature(), transferFunctions.data());
+        adj->profiles[1] = cmsCreateLinearizationDeviceLink(cmsSigGrayData, alphaTransferFunctions.data());
         adj->profiles[2] = 0;
         adj->csProfile = d->profile->lcmsProfile();
         adj->cmstransform  = cmsCreateTransform(adj->profiles[0], this->colorSpaceType(), 0, this->colorSpaceType(),
@@ -363,25 +346,25 @@ public:
                                   KoColorConversionTransformation::adjustmentRenderingIntent(),
                                   KoColorConversionTransformation::adjustmentConversionFlags());
 
-        delete [] transferFunctions;
-        delete [] alphaTransferFunctions;
         return adj;
     }
 
     quint8 difference(const quint8 *src1, const quint8 *src2) const override
     {
-        quint8 lab1[8], lab2[8];
-        cmsCIELab labF1, labF2;
+        std::array<cmsUInt16Number, 4> lab1{};
+        std::array<cmsUInt16Number, 4> lab2{};
+        cmsCIELab labF1;
+        cmsCIELab labF2;
 
         if (this->opacityU8(src1) == OPACITY_TRANSPARENT_U8
                 || this->opacityU8(src2) == OPACITY_TRANSPARENT_U8) {
             return (this->opacityU8(src1) == this->opacityU8(src2) ? 0 : 255);
         }
         Q_ASSERT(this->toLabA16Converter());
-        this->toLabA16Converter()->transform(src1, lab1, 1);
-        this->toLabA16Converter()->transform(src2, lab2, 1);
-        cmsLabEncoded2Float(&labF1, (cmsUInt16Number *)lab1);
-        cmsLabEncoded2Float(&labF2, (cmsUInt16Number *)lab2);
+        this->toLabA16Converter()->transform(src1, reinterpret_cast<quint8 *>(lab1.data()), 1);
+        this->toLabA16Converter()->transform(src2, reinterpret_cast<quint8 *> (lab2.data()), 1);
+        cmsLabEncoded2Float(&labF1, lab1.data());
+        cmsLabEncoded2Float(&labF2, lab2.data());
         qreal diff = cmsDeltaE(&labF1, &labF2);
 
         if (diff > 255.0) {
@@ -393,38 +376,31 @@ public:
 
     quint8 differenceA(const quint8 *src1, const quint8 *src2) const override
     {
-        quint8 lab1[8];
-        quint8 lab2[8];
+        std::array<cmsUInt16Number, 4> lab1 {};
+        std::array<cmsUInt16Number, 4> lab2 {};
         cmsCIELab labF1;
         cmsCIELab labF2;
 
         if (this->opacityU8(src1) == OPACITY_TRANSPARENT_U8
                 || this->opacityU8(src2) == OPACITY_TRANSPARENT_U8) {
-
-
             const qreal alphaScale = 100.0 / 255.0;
             return qRound(alphaScale * qAbs(this->opacityU8(src1) - this->opacityU8(src2)));
         }
         Q_ASSERT(this->toLabA16Converter());
-        this->toLabA16Converter()->transform(src1, lab1, 1);
-        this->toLabA16Converter()->transform(src2, lab2, 1);
-        cmsLabEncoded2Float(&labF1, (cmsUInt16Number *)lab1);
-        cmsLabEncoded2Float(&labF2, (cmsUInt16Number *)lab2);
+        this->toLabA16Converter()->transform(src1, reinterpret_cast<quint8 *> (lab1.data()), 1);
+        this->toLabA16Converter()->transform(src2, reinterpret_cast<quint8 *> (lab2.data()), 1);
+        cmsLabEncoded2Float(&labF1, lab1.data());
+        cmsLabEncoded2Float(&labF2, lab2.data());
 
-        cmsFloat64Number dL;
-        cmsFloat64Number da;
-        cmsFloat64Number db;
-        cmsFloat64Number dAlpha;
-
-        dL = fabs((qreal)(labF1.L - labF2.L));
-        da = fabs((qreal)(labF1.a - labF2.a));
-        db = fabs((qreal)(labF1.b - labF2.b));
+        cmsFloat64Number dL {fabs(static_cast<qreal>(labF1.L - labF2.L))};
+        cmsFloat64Number da {fabs(static_cast<qreal>(labF1.a - labF2.a))};
+        cmsFloat64Number db {fabs(static_cast<qreal>(labF1.b - labF2.b))};
 
         static const int LabAAlphaPos = 3;
         static const cmsFloat64Number alphaScale = 100.0 / KoColorSpaceMathsTraits<quint16>::max;
-        quint16 alpha1 = reinterpret_cast<quint16 *>(lab1)[LabAAlphaPos];
-        quint16 alpha2 = reinterpret_cast<quint16 *>(lab2)[LabAAlphaPos];
-        dAlpha = fabs((qreal)(alpha1 - alpha2)) * alphaScale;
+        quint16 alpha1 = lab1.at(LabAAlphaPos);
+        quint16 alpha2 = lab2.at(LabAAlphaPos);
+        cmsFloat64Number dAlpha {fabs((qreal)(alpha1 - alpha2)) * alphaScale};
 
         qreal diff = pow(dL * dL + da * da + db * db + dAlpha * dAlpha, 0.5);
 
@@ -445,13 +421,13 @@ private:
     inline static LcmsColorProfileContainer *asLcmsProfile(const KoColorProfile *p)
     {
         if (!p) {
-            return 0;
+            return nullptr;
         }
 
-        const IccColorProfile *iccp = dynamic_cast<const IccColorProfile *>(p);
+        const auto *iccp = dynamic_cast<const IccColorProfile *>(p);
 
         if (!iccp) {
-            return 0;
+            return nullptr;
         }
 
         Q_ASSERT(iccp->asLcms());
@@ -475,7 +451,7 @@ public:
 
     bool profileIsCompatible(const KoColorProfile *profile) const override
     {
-        const IccColorProfile *p = dynamic_cast<const IccColorProfile *>(profile);
+        const auto *p = dynamic_cast<const IccColorProfile *>(profile);
         return (p && p->asLcms()->colorSpaceSignature() == colorSpaceSignature());
     }
 
